2c24d24fc0605269432506d74c1de03ab0353614
Алексей <alexey@macbook-pro-aleksej.local>
2016-05-04 09:34:34 +0300

    Initial Commit

---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by Алексей on 04.05.16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSSReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSSReader
+//
+//  Created by Алексей on 04.05.16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
